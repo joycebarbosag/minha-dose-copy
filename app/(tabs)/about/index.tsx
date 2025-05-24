0c3eafb1bf0b1c91498cc954e7386111cb06eaac
Joyce Barbosa <86983255+joycebarbosag@users.noreply.github.com>
2025-05-24 19:43:59 +0000

    Base da pagina "About"

--- app/(tabs)/about/index.tsx
+++ app/(tabs)/about/index.tsx
@@ -1,30 +1,45 @@
+import PhotoCircle from "@/components/PhotoCircle";
 import { Link } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function About() {
     return (
         <View>
             <Text>Edit app/index.tsx to edit this screen.</Text>
-            <Link href="/about/bruno" push asChild>
-                <Button title="Clique para ver sobre Bruno"></Button>
+            <Link href="/about/bruno" asChild>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
-            <Link href="/about/douglas" push asChild>
-                <Button title="Clique para ver sobre Douglas"></Button>
+            <Link href="/about/douglas" asChild>
+               <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
             <Link href="/about/emmanuel" push asChild>
-                <Button title="Clique para ver sobre Emmanuel"></Button>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
-            <Link href="/about/isabela" push asChild>
-                <Button title="Clique para ver sobre Isabela"></Button>
+            <Link href="/about/isabela" asChild>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
-            <Link href="/about/joana" push asChild>
-                <Button title="Clique para ver sobre Joana"></Button>
+            <Link href="/about/joana" asChild>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
-                        <Link href="/about/joyce" push asChild>
-                <Button title="Clique para ver sobre Joyce"></Button>
+            <Link href="/about/joyce" asChild>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/joyce-photo.jpg')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
-            <Link href="/about/leonardo" push asChild>
-                <Button title="Clique para ver sobre Leonardo"></Button>
+            <Link href="/about/leonardo" asChild>
+                <Pressable>
+                    <PhotoCircle imageSource={require('@/assets/images/splash-icon.png')} size={100}></PhotoCircle>
+                </Pressable>
             </Link>
         </View>
     );
